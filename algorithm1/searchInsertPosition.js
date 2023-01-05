5d165fb8e33bc984f40bdae274e09702628d5cd8
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-01-05 15:42:22 +0800

    solution 3: searchInsertPosition

--- algorithm1/searchInsertPosition.js
+++ algorithm1/searchInsertPosition.js
@@ -3,14 +3,14 @@ const searchInsert = function (nums, target) {
         end = nums.length - 1;
 
     while (start <= end) {
-        const mid = Math.floor(start + (end - start) / 2);
+        const mid = Math.floor((start + end) / 2);
 
         if (nums[mid] === target) return mid;
         else if (nums[mid] > target) end = mid - 1;
         else start = mid + 1;
     }
 
-    return end + 1;
+    return start > end ? start : end;
 };
 
 console.log(searchInsert([1, 3, 5, 6], 4));
