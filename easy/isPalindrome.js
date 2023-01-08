85dc48e5f31a14c50415bdda498c123f4623d7e5
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-01-08 20:51:19 +0800

    add: O log 10 n solution

--- easy/isPalindrome.js
+++ easy/isPalindrome.js
@@ -2,5 +2,20 @@ const isPalindrome = function (x) {
     return x >= 0 && x.toString().split("").reverse().join("") == x;
 };
 
+const isPalindrome2 = function (x) {
+    if (x < 0 && x % 10 === 0 && x != 0) return false;
+
+    let reverseNumber = 0;
+
+    while (x > reverseNumber) {
+        reverseNumber = reverseNumber * 10 + (x % 10);
+        x = ~~(x / 10);
+    }
+
+    return x === reverseNumber || x === ~~(reverseNumber / 10);
+};
+
 console.log(isPalindrome(121));
 console.log(isPalindrome(0));
+
+console.log(isPalindrome2(12321));
