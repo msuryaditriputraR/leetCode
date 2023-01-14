4eea853c9c75ec180a205fdbb2e5e90c87878479
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-01-14 11:51:37 +0800

    solution 2

--- easy/removeElement.js
+++ easy/removeElement.js
@@ -6,7 +6,21 @@ var removeElement = function (nums, val) {
         }
     }
 
-    return nums;
+    return nums.length;
+};
+
+var removeElement2 = function (nums, val) {
+    let i = 0,
+        n = nums.length;
+
+    for (const num of nums) {
+        if (num !== val) {
+            i++;
+        }
+    }
+
+    return i;
 };
 
 console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
+console.log(removeElement2([0, 1, 2, 2, 3, 0, 4, 2], 2));
