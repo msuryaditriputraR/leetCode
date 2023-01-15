1913000f2c1a9153705a2551c37fc905daee86d9
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-01-15 14:13:01 +0800

    solve with for while

--- easy/lengthLastWord.js
+++ easy/lengthLastWord.js
@@ -2,4 +2,18 @@ const lengthLastWord = function (s) {
     return s.trim().split(" ").at(-1).length;
 };
 
+const lengthLastWord2 = function (s) {
+    let i = 0;
+    s = s.trim();
+
+    for (let j = s.length - 1; j >= 0; j--) {
+        if (s[j] === " ") return i;
+
+        i++;
+    }
+
+    return i;
+};
+
 console.log(lengthLastWord("   fly me   to   the moon  "));
+console.log(lengthLastWord2("   fly me   to   the moon  "));
